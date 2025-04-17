86550a166f9b8dd994668c68b101a15e694dc8b3
Gagan2004 <gagank90815@gmail.com>
2025-04-17 22:36:23 +0530

    render version

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use("/files", express.static(path.join(__dirname, "uploads")));
